254145127b0c68e07cb40d954c200ba0c47fd67d
mkoomvel <koomvelmoses@yahoo.com>
2015-10-02 16:09:46 +0100

    initial commit

---
+++ resources/views/blog/create.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title> SIMPLE FORM</title>
+	</head>
+		<body>
+			<center><h1>FORM </h1></center>
+					<form action = " ./form.php" method = "POST"/>
+					FIRST NAME&nbsp;&nbsp;&nbsp;<input name="fname" type="text"/>
+					<br>
+					LAST NAME&nbsp;&nbsp;&nbsp;&nbsp;<input name="lname" type="text"/>
+					<br>
+					ADDRESS&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input name="add" type="text"/>
+					<br>
+					AGE &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; <input name="age" type=" "/>
+					<br>
+
+					GENDER &nbsp;&nbsp;<input type="radio" name="sex" value = "male">male
+										<input type="radio" name="sex" value = "female">female
+										<br><br>
+					<input type="submit" value="login" name="sub"/>
+					<br>
+					</form>
+
+		</body>
+	
+</html>
---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>simple</title>
+	<Style>
+#header{
+background-color: #eee;
+color: black;
+margin: 50px;
+padding: 30px;
+border-top-left-radius: 40px;
+border-top-right-radius:40px;
+}
+#nav{
+	background-color: white;
+	float: left;
+	height: 300px;
+	width: 200px;
+	line-height: 50px;
+	border-radius: 40px;
+	text-align: center;
+}
+#section{
+	background-color:#eee;
+	height: 50px;
+	width: 
+}
+#mo{
+	background-color: #eee;
+	text-align: right;
+	padding: 100px;
+}
+	#footer{
+		background-color: #eee;
+		text-align: right;
+		padding: 5px;
+		margin-top: 20px;
+	}
+	</Style>
+</head>
+<body>
+<div id="header">LOGO</div><hr>
+		<div id="nav">
+			<a href="">EVENT</a><br>
+			<a href="">About Us</a><br>
+			<a href="">Gallary</a><br>
+			<a href="">Contact</a><br>
+		</div>
+	<div id="section">
+		<fieldset>
+		<legend>Discription</legend>
+		</fieldset>
+	</div>	
+
+		<img src="/images/emoor.jpeg" alt="banner" width="120" height="70">
+			<div id="mo">
+				login<input type="text" value="login"><br>
+				password<input type="password" value="password"><br>&nbsp&nbsp&nbsp
+				&nbsp&nbsp&nbsp<a href="">Sign up?</a>
+			</div>
+
+			<div id="footer">
+				@emoor 2015
+			</div>																														
+</body>
+</html>
